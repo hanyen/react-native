eca3f6087529cab1e01aedf5bcc4d1c8220cad2e
hanyen <me@hanyen.com>
2017-08-18 15:29:41 -0700

    Nest header component inside app

--- index.ios.js
+++ index.ios.js
@@ -2,12 +2,13 @@
 
 //import a library to help create a component
 import React from 'react'; //how the component should behave
-import { Text, AppRegistry } from 'react-native'; //take output from component and display it
+import { AppRegistry } from 'react-native'; //take output from component and display it
+import Header from './src/components/header';
 
 //create a component
 const App = () => {
   return (
-    <Text>Some Text</Text>
+      <Header />
   );
 };
 
