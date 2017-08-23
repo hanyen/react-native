618944ec3096fcfa85042f659c7f851b796bd2f3
hanyen <me@hanyen.com>
2017-08-23 12:38:53 -0700

    Render album list object properties

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -11,11 +11,15 @@ class AlbumList extends React.Component {   //refactor to class declaration
     Axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => this.setState({ albums: response.data })); //pass the object to setState function
   }
   
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
+
   render() {
-    console.log(this.state);
+    // console.log(this.state);
     return (
     <View>
-      <Text>Album List</Text>
+      {this.renderAlbums()}
     </View>
     );  
   }
